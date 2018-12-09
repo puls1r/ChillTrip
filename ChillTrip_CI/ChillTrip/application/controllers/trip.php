fcec29b2779eaf72a96d64ac1cfc68a60303cd4b
Hanif <haniefm12@gmail.com>
2018-12-09 09:55:56 +0700

    terbaru

--- ChillTrip_CI/ChillTrip/application/controllers/trip.php
+++ ChillTrip_CI/ChillTrip/application/controllers/trip.php
@@ -1,14 +1,13 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
  
-class Home extends CI_Controller {
+class Trip extends CI_Controller {
 	
-	function __construct(){
+    function __construct(){
 		parent::__construct();
 		$this->load->helper('url');
 	}
- 
-	public function index(){		
+    public function index(){		
 		$data['judul'] = "Halaman trip";
 		$this->load->view('v_trip',$data);
 	}
--- ChillTrip_CI/ChillTrip/application/views/v_trip.php
+++ ChillTrip_CI/ChillTrip/application/views/v_trip.php
@@ -1,11 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Chill Trip | Home</title>
+<title>Chill Trip | Trip</title>
 <link href="<?php echo base_url() ?>assets/bootstrap-4.1.3-dist/css/bootstrap-grid.css" rel="stylesheet" type="text/css">
 <link href="<?php echo base_url() ?>assets/css/gaya.css" rel="stylesheet" type="text/css">
 <link href="<?php echo base_url() ?>assets/css/footer.css" rel ="stylesheet" type="text/css">
-
 <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
 </head>
 
@@ -13,50 +12,21 @@
 <div class="container-fluid" style="padding:0;margin:0">
 	<div class="navbar row">
 		<div class="nav col-sm-2 col-xs-2 col-md-2 col-lg-2"><a style="padding:10px">Chill <span style="color:#c12500">Trip<span></a></div>
-		<div class="nav col-sm-2 col-xs-2 col-md-2 col-lg-2"><a href="<?php echo site_url('home') ?>" class="active" style="padding:10px">Home</a></div>
-		<div class="nav col-sm-2 col-xs-2 col-md-2 col-lg-2"><a href="<?php echo site_url('trip') ?>" style="padding:10px">Trip</a></div>
+		<div class="nav col-sm-2 col-xs-2 col-md-2 col-lg-2"><a href="<?php echo site_url('home') ?>" style="padding:10px">Home</a></div>
+		<div class="nav col-sm-2 col-xs-2 col-md-2 col-lg-2"><a href="<?php echo site_url('trip') ?>" class="active" style="padding:10px">Trip</a></div>
 		<div class="nav col-sm-2 col-xs-2 col-md-2 col-lg-2"><a href="<?php echo site_url('schedule') ?>" style="padding:10px">Schedule</a></div>
 		<div class="nav col-sm-2 col-xs-2 col-md-2 col-lg-2"><a href="<?php echo site_url('chart') ?>" style="padding:10px">Chart</a></div>
 		<div class="nav col-sm-2 col-xs-2 col-md-2 col-lg-2"><a href="<?php echo site_url('login') ?>"style="padding:10px">Login</a></div>
 	</div>
-	<div class="intro row" style= background-color:#34dddd>
-	<div class="col-sm-4 col-xs-4 col-md-4 col-lg-4"><img src="<?php echo base_url() ?>assets/img/indah.png" alt="Logo" style="height:250px; width:250x;"></div>
-	<div class="text col-sm-8 col-xs-8 col-md-8 col-lg-8"><img class="logo" src="<?php echo base_url() ?>assets/img/logo/chillTrip_logo.png">
-<p style="color:#c12500">"We Chill Every Trip:)"</p>
-	<p>Find the best deal for every trip you want!</p></div>
-	</div>
-	<div class="how row" style="margin:0">
-	<div class="col-sm-12 col-xs-12 col-md-12 col-lg-12" style="margin:0;"><h1 style="text-align:center">How to Chill With us</h1>
-	<table class="spacing" cellpadding="2px">
-	<tr>
-		<td>1. </td><td>Create an account.</td>
-	</tr>
-	<tr>
-		<td></td><td>Click here to create an account</td>
-	</tr>
-	<tr>
-		<td><br>2. </td><td><br>Go to "Trip"</td>
-	</tr>
-	<tr>
-		<td><br>3. </td><td><br>Choose your destination</td>
-	</tr>
-	<tr>
-		<td></td><td>and your holiday date</td>
-	</tr>
-	<tr>
-		<td><br>4. </td><td><br>Book your chill trip</td>
-	</tr>
-	<tr>
-		<td><br>5. </td><td><br>Go to "Chart" and make the payment</td>
-	</tr>
-	<tr>
-		<td><br>6. </td><td><br>Print the payment invoice</td>
-	</tr>
-	<tr>
-		<td><br>7. </td><td><br>Receive Your E-Ticket</td>
-	</tr>
-	</table>
-	</div>
+	<h1 style="text-align:left; margin-left:100px;">Most Popular City</h1>
+	<div class="row">
+		<div class="nav col-sm-1 col-xs-1 col-md-1 col-lg-1"></div>
+		<div class="nav col-sm-7 col-xs-7 col-md-7 col-lg-7"><img class="center" src="<?php echo base_url() ?>assets/img/bandung.jpg" alt="Logo" style="height:500px; width:730px;padding:5px"></div>
+		<div class="nav col-sm-3 col-xs-3 col-md-3 col-lg-3">
+			<div class="row"><div class="nav col-sm-12 col-xs-12 col-md-12 col-lg-12"><img class="center" src="<?php echo base_url() ?>assets/img/wonosobo.jpg" alt="Logo" style="height:250px; width:315px;padding:5px"></div></div>
+			<div class="row"><div class="nav col-sm-12 col-xs-12 col-md-12 col-lg-12"><img class="center" src="<?php echo base_url() ?>assets/img/seribu.jpg" alt="Logo" style="height:250px; width:315px;padding:5px"></div></div>	
+		</div>
+		<div class="nav col-sm-1 col-xs-1 col-md-1 col-lg-1"></div>
 	</div>
 	<div class="row" style="margin:0">
 	<div class="col-sm-12 col-xs-12 col-md-12 col-lg-12" style="padding:0">
@@ -113,6 +83,4 @@
 		</div>
 </div>
 </body>
-
-
 </html>
