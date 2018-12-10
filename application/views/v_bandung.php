071dfdaee88b2dc01485cd7e186fd574c9e83c83
adorabable <whatthefapmc@gmail.com>
2018-12-10 20:53:14 +0700

    sample ticket page

--- application/views/v_bandung.php
+++ application/views/v_bandung.php
@@ -5,6 +5,7 @@
 <link href="<?php echo base_url() ?>assets/bootstrap-4.1.3-dist/css/bootstrap-grid.css" rel="stylesheet" type="text/css">
 <link href="<?php echo base_url() ?>assets/css/gaya.css" rel="stylesheet" type="text/css">
 <link href="<?php echo base_url() ?>assets/css/footer.css" rel ="stylesheet" type="text/css">
+<link href="<?php echo base_url() ?>assets/css/paket.css" rel ="stylesheet" type="text/css">
 <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
 </head>
 
@@ -13,9 +14,102 @@
 	<div class="navbar row">
 		<div class="nav col-sm-2 col-xs-2 col-md-2 col-lg-2"><a style="padding:10px">Chill <span style="color:#c12500">Trip<span></a></div>
 		<div class="nav col-sm-2 col-xs-2 col-md-2 col-lg-2"><a href="<?php echo site_url('home') ?>" style="padding:10px">Home</a></div>
-		<div class="nav col-sm-2 col-xs-2 col-md-2 col-lg-2"><a href="<?php echo site_url('trip') ?>" style="padding:10px">Trip</a></div>
+		<div class="nav col-sm-2 col-xs-2 col-md-2 col-lg-2"><a href="<?php echo site_url('trip') ?>" class="active" style="padding:10px">Trip</a></div>
 		<div class="nav col-sm-2 col-xs-2 col-md-2 col-lg-2"><a href="<?php echo site_url('chart') ?>" style="padding:10px">Chart</a></div>
 		<div class="nav col-sm-2 col-xs-2 col-md-2 col-lg-2"><a style="padding:10px">login session</a></div>
 		<div class="nav col-sm-2 col-xs-2 col-md-2 col-lg-2"><a href="<?php echo site_url('login') ?>"style="padding:10px">Login</a></div>
+	</div>
+	<div class="row">
+		<div class="nav col-sm-12 col-xs-12 col-md-12 col-lg-12"><h1 style="text-align:center">Bandung</h1></div>
+	</div><div class="row">
+		<div class="col-sm-2 col-xs-2 col-md-2 col-lg-2"></div>
+		<div class="col-sm-8 col-xs-8 col-md-8 col-lg-8"><p style="color:grey;font-size:30px;margin:0;margin-top:15px">Package</p><p style="color:grey;margin:0;margin-top:10px;margin-bottom:20px">around Bandung</p></div>
+		<div class="col-sm-2 col-xs-2 col-md-2 col-lg-2"></div>
+	</div>
+	<div class="row">
+		<div class="col-sm-2 col-xs-2 col-md-2 col-lg-2"></div>
+		<div class="col-sm-8 col-xs-8 col-md-8 col-lg-8" style="border:none;border-top:solid 1px grey">
+			<div class="row">
+				<div class="paket col-sm-12 col-xs-12 col-md-12 col-lg-12">
+					<h4 style="margin:0;color:grey">ABC Travel</h4>
+					<h6 style="margin-top:2px;color:grey">Executive</h6>
+					<div class="row">
+						<div class="col-sm-2 col-xs-2 col-md-2 col-lg-2"><p style="color:grey;margin:0"><b>08.00</b></p><p style="color:grey;margin:0;margin-top:3px">Buah Batu</p></div>
+						<div class="col-sm-2 col-xs-2 col-md-2 col-lg-2"><p style="color:grey;margin:0"><b>10.00</b></p><p style="color:grey;margin:0;margin-top:3px">Ciater</p></div>
+						<div class="col-sm-2 col-xs-2 col-md-2 col-lg-2"><p style="color:grey;margin:0"><b>12.00</b></p><p style="color:grey;margin:0;margin-top:3px">Mt. Tangkuban Perahu</p></div>
+						<div class="col-sm-3 col-xs-3 col-md-3 col-lg-3"></div>
+						<div class="col-sm-3 col-xs-3 col-md-3 col-lg-3"><p style="margin:0">Total :</p><h2 style="margin-top:2px">Rp. 300.000</h2></div>
+					</div>
+				</div>				
+			</div>
+			<div class="row">
+				<div class="paket col-sm-12 col-xs-12 col-md-12 col-lg-12">
+					<h4 style="margin:0;color:grey">Naga</h4>
+					<h6 style="margin-top:2px;color:grey">Super Executive</h6>
+					<div class="row">
+						<div class="col-sm-2 col-xs-2 col-md-2 col-lg-2"><p style="color:grey;margin:0"><b>06.00</b></p><p style="color:grey;margin:0;margin-top:3px">Ciater</p></div>
+						<div class="col-sm-2 col-xs-2 col-md-2 col-lg-2"><p style="color:grey;margin:0"><b>08.00</b></p><p style="color:grey;margin:0;margin-top:3px">Kawah Putih</p></div>
+						<div class="col-sm-2 col-xs-2 col-md-2 col-lg-2"><p style="color:grey;margin:0"><b>12.00</b></p><p style="color:grey;margin:0;margin-top:3px">Dago</p></div>
+						<div class="col-sm-3 col-xs-3 col-md-3 col-lg-3"></div>
+						<div class="col-sm-3 col-xs-3 col-md-3 col-lg-3"><p style="margin:0">Total :</p><h2 style="margin-top:2px">Rp. 300.000</h2></div>
+					</div>
+				</div>				
+			</div>	
+		</div>
+		<div class="col-sm-2 col-xs-2 col-md-2 col-lg-2"></div>
+	</div>
+	<div class="row" style="margin:0">
+	<div class="col-sm-12 col-xs-12 col-md-12 col-lg-12" style="padding:0">
+	<footer class="footer-distributed">
+
+			<div class="footer-left">
+
+				<img class="logo" src="<?php echo base_url() ?>assets/img/logo/chillTrip_logo.png">
+
+				
+
+				<p class="footer-company-name">Chill Trip &copy; 2018</p>
+			</div>
+
+			<div class="footer-center">
+
+				<div>
+					<i class="fa fa-map-marker"></i>
+					<p><span>Komp. Puri indah Blok B5 no. 17, Jatinangor</span> Jawa Barat, Indonesia</p>
+				</div>
+
+				<div>
+					<i class="fa fa-phone"></i>
+					<p>+0878 7877 0097</p>
+				</div>
+
+				<div>
+					<i class="fa fa-envelope"></i>
+					<p><a href="mailto:cs@opentrip.com">cs@chilltrip.com</a></p>
+				</div>
+
+			</div>
+
+			<div class="footer-right">
+
+				<p class="footer-company-about">
+					<span>About us</span>
+					Chill Trip is a Website that provides you choices to fullfill your holiday wishes
+				</p>
+
+				<div class="footer-icons">
+
+					<a href="#"><i class="fa fa-facebook"></i></a>
+					<a href="#"><i class="fa fa-twitter"></i></a>
+					<a href="#"><i class="fa fa-linkedin"></i></a>
+					<a href="#"><i class="fa fa-github"></i></a>
+
+				</div>
+
+			</div>
+
+		</footer>
+		</div>
+		</div>
 	</div>
 </body>
